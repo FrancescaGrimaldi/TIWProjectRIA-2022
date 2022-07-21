e040fc2dbe08119ae689c46f9462ad0053a1a585
robertogiandomenico <roberto.giandomenico@mail.polimi.it>
2022-07-21 17:13:08 +0200

    updated tables functions

--- TIWProjectRIA-2022/src/main/webapp/homepageController.js
+++ TIWProjectRIA-2022/src/main/webapp/homepageController.js
@@ -26,23 +26,27 @@
 	function CreatedMeeting(_alert, _place) {
 		this.alert = _alert;
 		this.place = _place;
-		this.meetings = _meetings;
+
+		this.reset = function() {
+			this.place.style.display = "none";
+		};
 
 		this.show = function(next) {
-			var self = this;
+			const self = this;
 			makeCall("GET", "GetCreatedMeetings", null,
 				function(req) {
 					if (req.readyState == 4) {
-						var message = req.responseText;
+						let message = req.responseText;
 						if (req.status == 200) {
 							var meetingsToShow = JSON.parse(req.responseText);
 							if (meetings == null || meetings.size() == 0) {   //check the variable type of meetings
-								let text = document.createElement("h4");
-								text.textContent = "You haven't created any meeting";
-								place.appendChild(text);
+								//let text = document.createElement("h4");
+								place.textContent = "You haven't created any meeting";
+								//place.appendChild(text);
 								return;
 							}
-							var meetTable = document.createElement('table');
+							let meetTable = document.createElement('table');
+							meetTable.border = "1px soldid black";
 							self.update(meetingsToShow, meetTable); // self visible by closure
 
 						} else if (req.status == 403) {
@@ -59,15 +63,13 @@
 		this.update = function(_meetingsToShow, _meetTable) {
 			this.meetingsToShow = _meetingsToShow;
 			this.meetTable = _meetTable;
-			
+
 			let self = this;
 			let tr = document.createElement('tr');
 			let tabFields = ['Title', 'Date', 'Time', 'Duration', 'Maximum number of partecipants'];
 			let th, text;
-			let tr, td1, td2, td3, td4, td5;
-			let title, date, time, duration, maxParts;
-			
-			self.documentscontainer.innerHTML = "";
+			let td1, td2, td3, td4, td5;
+			//let title, date, time, duration, maxParts;
 
 			for (var i = 0; i < tabFields.length; i++) {
 				th = document.createElement('th'); 			//column
@@ -78,7 +80,7 @@
 			meetTable.appendChild(tr);
 
 
-			meetingsToShow.forEach(function(meet) {
+			meetingsToShow.forEach(function(meet) {		// 'this' is not visible here, 'self' is
 				tr = document.createElement('tr');
 
 				td1 = document.createElement('td');
@@ -87,17 +89,17 @@
 				td4 = document.createElement('td');
 				td5 = document.createElement('td');
 
-				title = document.createTextNode(meet.title);    	//da modificare con i tipi di variable giusto
+				/*title = document.createTextNode(meet.title);    	//da modificare con i tipi di variable giusto
 				date = document.createTextNode(meet.date);			//da modificare con i tipi di variable giusto
 				time = document.createTextNode(meet.time);			//da modificare con i tipi di variable giusto
 				duration = document.createTextNode(meet.duration);	//da modificare con i tipi di variable giusto
-				maxParts = document.createTextNode(meet.maxParts);	//da modificare con i tipi di variable giusto
+				maxParts = document.createTextNode(meet.maxParts);	//da modificare con i tipi di variable giusto*/
 
-				td1.appendChild(title);
-				td2.appendChild(date);
-				td3.appendChild(time);
-				td4.appendChild(duration);
-				td5.appendChild(maxParts);
+				td1.textContent = meet.title;    	//da modificare con i tipi di variable giusto??
+				td2.textContent = meet.date;
+				td3.textContent = meet.time;
+				td4.textContent = meet.duration;
+				td5.textContent = meet.maxParts;
 
 				tr.appendChild(td1);
 				tr.appendChild(td2);
@@ -113,64 +115,102 @@
 	}
 
 
-	function InvitedMeeting(place) {
 
-		if (meetings == null || meetings.size() == 0) {   //check the variable type of meetings
-			let text = document.createElement("h4");
-			text.textContent = "You haven't been invited to any meeting";
-			place.appendChild(text);
-			return;
+
+
+	function InvitedMeeting(_alert, _place) {
+		this.alert = _alert;
+		this.place = _place;
+
+		this.reset = function() {
+			this.place.style.display = "none";
+		};
+
+		this.show = function(next) {
+			const self = this;
+			makeCall("GET", "GetInvitedMeetings", null,
+				function(req) {
+					if (req.readyState == 4) {
+						let message = req.responseText;
+						if (req.status == 200) {
+							var meetingsToShow = JSON.parse(req.responseText);
+							if (meetings == null || meetings.size() == 0) {   //check the variable type of meetings
+								//let text = document.createElement("h4");
+								place.textContent = "You haven't been invited to any meeting";
+								//place.appendChild(text);
+								return;
+							}
+							let meetTable = document.createElement('table');
+							meetTable.border = "1px soldid black";
+							self.update(meetingsToShow, meetTable); // self visible by closure
+
+						} else if (req.status == 403) {
+							window.sessionStorage.removeItem('user');
+						}
+						else {
+							self.alert.textContent = message;
+						}
+					}
+				}
+			);
+		};
+
+		this.update = function(_meetingsToShow, _meetTable) {
+			this.meetingsToShow = _meetingsToShow;
+			this.meetTable = _meetTable;
+
+			let self = this;
+			let tr = document.createElement('tr');
+			let tabFields = ['Title', 'Date', 'Time', 'Duration', 'Max partecipants', 'Creator'];
+			let th, text;
+			let td1, td2, td3, td4, td5, td6;
+			//let title, date, time, duration, maxParts;
+
+			for (var i = 0; i < tabFields.length; i++) {
+				th = document.createElement('th'); 			//column
+				text = document.createTextNode(array[i]); 	//column title
+				th.appendChild(text);
+				tr.appendChild(th);
+			}
+			meetTable.appendChild(tr);
+
+
+			meetingsToShow.forEach(function(meet) {		// 'this' is not visible here, 'self' is
+				tr = document.createElement('tr');
+
+				td1 = document.createElement('td');
+				td2 = document.createElement('td');
+				td3 = document.createElement('td');
+				td4 = document.createElement('td');
+				td5 = document.createElement('td');
+				td6 = document.createElement('td');
+
+				/*title = document.createTextNode(meet.title);    	//da modificare con i tipi di variable giusto
+				date = document.createTextNode(meet.date);			//da modificare con i tipi di variable giusto
+				time = document.createTextNode(meet.time);			//da modificare con i tipi di variable giusto
+				duration = document.createTextNode(meet.duration);	//da modificare con i tipi di variable giusto
+				maxParts = document.createTextNode(meet.maxParts);	//da modificare con i tipi di variable giusto
+				creator = document.createTextNode(meet.creator);	//da modificare con i tipi di variable giusto*/
+
+				td1.textContent = meet.title;    	//da modificare con i tipi di variable giusto??
+				td2.textContent = meet.date;
+				td3.textContent = meet.time;
+				td4.textContent = meet.duration;
+				td5.textContent = meet.maxParts;
+				td6.textContent = meet.creator;
+
+				tr.appendChild(td1);
+				tr.appendChild(td2);
+				tr.appendChild(td3);
+				tr.appendChild(td4);
+				tr.appendChild(td5);
+				tr.appendChild(td6);
+
+				table.appendChild(tr);
+			})
+
+			place.appendChild(meetTable);
 		}
-
-		var meetTable = document.createElement('table');
-		var tr = document.createElement('tr');
-		var tabFields = ['Title', 'Date', 'Time', 'Duration', 'Max partecipants', 'Creator'];
-
-		for (var i = 0; i < tabFields.length; i++) {
-			var th = document.createElement('th'); 			//column
-			var text = document.createTextNode(array[i]); 	//column title
-			th.appendChild(text);
-			tr.appendChild(th);
-		}
-		meetTable.appendChild(tr);
-
-
-
-		for (let meet of meetings.children) {   //da modificare con i tipi di variable giusto e lista giusta
-			var tr = document.createElement('tr');
-
-			var td1 = document.createElement('td');
-			var td2 = document.createElement('td');
-			var td3 = document.createElement('td');
-			var td4 = document.createElement('td');
-			var td5 = document.createElement('td');
-			var td6 = document.createElement('td');
-
-			var title = document.createTextNode(meet.title);    	//da modificare con i tipi di variable giusto
-			var date = document.createTextNode(meet.date);			//da modificare con i tipi di variable giusto
-			var time = document.createTextNode(meet.time);			//da modificare con i tipi di variable giusto
-			var duration = document.createTextNode(meet.duration);	//da modificare con i tipi di variable giusto
-			var maxParts = document.createTextNode(meet.maxParts);	//da modificare con i tipi di variable giusto
-			var creator = document.createTextNode(meet.creator);	//da modificare con i tipi di variable giusto
-
-			td1.appendChild(title);
-			td2.appendChild(date);
-			td3.appendChild(time);
-			td4.appendChild(duration);
-			td5.appendChild(maxParts);
-			td6.appendChild(creator);
-
-			tr.appendChild(td1);
-			tr.appendChild(td2);
-			tr.appendChild(td3);
-			tr.appendChild(td4);
-			tr.appendChild(td5);
-			tr.appendChild(td6);
-
-			table.appendChild(tr);
-		}
-
-		place.appendChild(meetTable);
 	}
 
 	function minDate() {
@@ -196,6 +236,7 @@
 			// init welcome message
 			welcomeMessage = new WelcomeMessage(sessionStorage.getItem("username"), document.getElementById("welcomeMessage"));
 			welcomeMessage.show();
+			
 
 			// handles logout
 			document.getElementById("logoutButton").addEventListener('click', () => {
@@ -209,12 +250,15 @@
 			);
 
 			// init invited meeting table
-			invitedMeeting = new InvitedMeeting(document.getElementById("invitedMeetingArea"));
+			invitedMeeting = new InvitedMeeting(
+				document.getElementById("genericMeetingError"),
+				document.getElementById("invitedMeetingArea")
+			);
 
 			//↓ ↓ ↓ ↓ ↓ ↓ ↓   TO ADAPT OR DELETE   ↓ ↓ ↓ ↓ ↓ ↓ ↓
-			// register folder_form wizard
+			/* register folder_form wizard
 			wizard = new Wizard(document.getElementById("create-content"));
-
+			*/
 		};
 
 		this.refresh = function() {
